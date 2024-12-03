15d2aba691bd9cb40bbc33bfcdb0804089cea045
Isaac Dolphin <isaac_dolphin@outlook.com>
2024-12-03 17:39:43 +0000

    Fixed OTP 🙌 added prettier formatting file.

--- components/auth/login/LoginForm.tsx
+++ components/auth/login/LoginForm.tsx
@@ -5,11 +5,9 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoginSchema } from "@/components/auth/login/LoginSchema";
-import { OTPSchema } from "@/components/auth/login/OTPSchema";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -17,30 +15,22 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { login, verifyOtp } from "@/app/auth/actions";
-import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
-import { REGEXP_ONLY_DIGITS } from "input-otp";
+import { login } from "@/app/auth/actions";
+import OTPForm from "@/components/auth/otp/OTPForm";
 
 export default function LoginForm() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [email, setEmail] = useState("");
   const [isOtpSent, setIsOtpSent] = useState(false);
 
-  const loginForm = useForm<z.infer<typeof LoginSchema>>({
+  const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: "",
     },
   });
 
-  const otpForm = useForm<z.infer<typeof OTPSchema>>({
-    resolver: zodResolver(OTPSchema),
-    defaultValues: {
-      pin: "",
-    },
-  });
-
-  const handleLoginSubmit = async (data: z.infer<typeof LoginSchema>) => {
+  const handleSubmit = async (data: z.infer<typeof LoginSchema>) => {
     setIsSubmitting(true);
     try {
       await login(data.email);
@@ -51,28 +41,26 @@ export default function LoginForm() {
     }
   };
 
-  const handleOtpSubmit = async (data: z.infer<typeof OTPSchema>) => {
-    setIsSubmitting(true);
-    try {
-      await verifyOtp(email, data.pin);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
   return (
-    <div>
+    <>
       {!isOtpSent ? (
-        <Form {...loginForm}>
-          <form onSubmit={loginForm.handleSubmit(handleLoginSubmit)} className="grid gap-4">
+        <Form {...form}>
+          <form 
+            onSubmit={form.handleSubmit(handleSubmit)} 
+            className="grid gap-4"
+          >
             <FormField
-              control={loginForm.control}
+              control={form.control}
               name="email"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input placeholder="me@example.com" {...field} required />
+                    <Input 
+                      placeholder="me@example.com" 
+                      {...field} 
+                      required 
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -83,44 +71,7 @@ export default function LoginForm() {
             </Button>
           </form>
         </Form>
-      ) : (
-        <Form {...otpForm}>
-          <form onSubmit={otpForm.handleSubmit(handleOtpSubmit)} className="space-y-6">
-            <FormField
-              control={otpForm.control}
-              name="pin"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>One-time Password</FormLabel>
-                  <FormControl>
-                    <InputOTP 
-                      maxLength={6}
-                      pattern={REGEXP_ONLY_DIGITS} 
-                      {...field}
-                    >
-                      <InputOTPGroup>
-                        <InputOTPSlot index={0} />
-                        <InputOTPSlot index={1} />
-                        <InputOTPSlot index={2} />
-                        <InputOTPSlot index={3} />
-                        <InputOTPSlot index={4} />
-                        <InputOTPSlot index={5} />
-                      </InputOTPGroup>
-                    </InputOTP>
-                  </FormControl>
-                  <FormDescription>
-                    Please enter the one-time password sent to your email.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button loading={isSubmitting} type="submit">
-              {isSubmitting ? "Verifying..." : "Verify OTP"}
-            </Button>
-          </form>
-        </Form>
-      )}
-    </div>
-  );
+      ) : <OTPForm email={email} /> }
+    </>
+  )
 };
