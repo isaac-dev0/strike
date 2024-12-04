8a0e90feccb59f8e44ad4cf38b4ab44849d5545f
Isaac Dolphin <d044286m@student.staffs.ac.uk>
2024-12-04 23:51:57 +0000

    Created table components for referrals, modified styling, created typing for referrals.

---
+++ components/referrals/ReferralStatusBadge.tsx
@@ -0,0 +1,26 @@
+import { ReferralStatus } from "@/enums/ReferralStatus";
+import { Badge } from "../ui/badge";
+
+interface StatusBadgeProps {
+  status: ReferralStatus;
+}
+
+const statusColours = {
+  [ReferralStatus.RECEIVED]: "bg-status-received text-status-received-foreground",
+  [ReferralStatus.ACCEPTED]: "bg-status-accepted text-status-accepted-foreground",
+  [ReferralStatus.DECLINED]: "bg-status-declined text-status-declined-foreground",
+  [ReferralStatus.IN_PROGRESS]: "bg-status-in-progress text-status-in-progress-foreground",
+  [ReferralStatus.READY]: "bg-status-ready text-status-ready-foreground",
+  [ReferralStatus.PENDING]: "bg-status-pending text-status-pending-foreground",
+  [ReferralStatus.COLLECTED]: "bg-status-collected text-status-collected-foreground"
+};
+
+export const ReferralStatusBadge: React.FC<StatusBadgeProps> = ({ status }) => {
+  const colourClass = statusColours[status] || "bg-gray-500 text-white";
+
+  return (
+    <Badge className={`${colourClass}`}>
+      {status}
+    </Badge>
+  )
+}
---
+++ components/referrals/table/ReferralTableHeader.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function ReferralTableHeader() {
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center">
+        <Button>
+          Create New
+        </Button>
+      </div>
+    </div>
+  )
+}
