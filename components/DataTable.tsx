d5dafbd8b866d1b2714eebde347d98b1907cc43c
Isaac Dolphin <d044286m@student.staffs.ac.uk>
2024-12-07 20:06:08 +0000

    Fixed bug with selecting checkboxes and it's message state

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -151,7 +151,7 @@ export default function DataTable<TData, TValue>({
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
-        {table.getIsSomePageRowsSelected() && (
+        {(table.getIsSomePageRowsSelected() || table.getIsAllPageRowsSelected()) && (
           <div className="flex-1 text-sm text-muted-foreground">
             {table.getFilteredSelectedRowModel().rows.length} of{" "}
             {table.getFilteredRowModel().rows.length} row(s) selected.
